cb9bff3f360c190581be4c5a57ca41f0bd24d142
Edisolt5498 <edisolt5498@gmail.com>
2022-11-22 04:09:47 +0300

    Нужно исправить вывод отрицательных чисел при вводе римских и настроить исключения

--- src/Tester.java
+++ src/Tester.java
@@ -1,122 +1,103 @@
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Tester {
-
     public static void main(String[] args) {
+
+        String[] BookRoman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV",
+                "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX",
+                "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII",
+                "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII",
+                "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII",
+                "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI",
+                "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };
 
         Scanner in = new Scanner(System.in);
         System.out.print("Input ");
 
-        String test = in.nextLine();
-
-        String[] corrected = test.split(" ");   //разбить на 3 строки
-        //String[] corrected = test.split("[+-/*]");
+        String InputUser = in.nextLine().replaceAll(" ", "").trim();    // строка без пробелов
 
 
-        char operator;
-        int num1 = 0;
-        int num2 = 0;
-        int sum = 10000;
-        String[] Book = {"A", "B", "C"};    //temp. Тут будет словарь римских чисел
+        for (int i = 0; i < InputUser.length(); i++) {
 
+            if ((InputUser.charAt(i)) == '+' || (InputUser.charAt(i)) == '-' || (InputUser.charAt(i)) == '*' || (InputUser.charAt(i)) == '/') {
+                char operator = InputUser.charAt(i);                                                // проверка на наличие знака и сохранение знака и потенциальных чисел
+                String num1 = InputUser.substring(0, i);
+                String num2 = InputUser.substring(i + 1, InputUser.length());
 
-        if (corrected.length > 3){
-            System.out.println("неверный формат... длина"); // проверка на количество введенных данных
-            System.exit(-4);
-        }
+                if (checker(new String[]{num1, num2}, BookRoman, "неверноый формат...")) {   // проверка на арабские и на ошибки (метод не даст ввести числа разных систем)
+                    System.out.println(num1 + " " + operator + " " +num2 + " = " + ArabCalc(new String[]{num1, num2}, operator));               // калькулятор для арабских чисел
+                } else {
+                    System.out.println(num1 + " " + operator + " " +num2 + " = " + RomanCalc(new String[]{num1, num2}, BookRoman, operator));   // калькулятор для римских чисел
+                }
 
-
-        operator = corrected[1].charAt(0);  // оператор стоит вторым, считываем его
-
-
-        if (checker(corrected, "неверноый формат...")) {    // проверка на введенные данные числа и знаки и определение формата чисел. Если арабские, то считаем
-            num1 = Integer.parseInt(corrected[0]);
-            num2 = Integer.parseInt(corrected[2]);
-            switch (operator) {
-                case '+' -> sum = num1 + num2;
-                case '-' -> sum = num1 - num2;
-                case '*' -> sum = num1 * num2;
-                case '/' -> sum = num1 / num2;
             }
-        } else if (!checker(corrected, "неверноый формат...")) {    // если римсиие, то считаем в классе
-            System.out.println(RomanCalc(corrected, Book, operator));   //передаем инпут, словарь, и оператор и получаем стринг ответа в римских числах
+
         }
 
-
-        /*System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(sum);*/
-
+        System.out.println("Где знак?...");                                                 //ГДЕ ОН?
+        System.exit(-4);
 
     }
 
+    public static boolean checker (String[] numbs, String[] Book, String massage) {
 
-    public static boolean checker (String[] Food, String massage) {
-
-        boolean Arab = false;
         byte check = 0;
+        //boolean Arab = false;
 
-        for (int i = 0; i < 3; i++) {
-
-                if ((Objects.equals(Food[i], "1") || Objects.equals(Food[i], "2") || Objects.equals(Food[i], "3") || Objects.equals(Food[i], "4")
-                        || Objects.equals(Food[i], "5") || Objects.equals(Food[i], "6") || Objects.equals(Food[i], "7")
-                        || Objects.equals(Food[i], "8") || Objects.equals(Food[i], "9") || Objects.equals(Food[i], "10")) && i != 1)
-                {
-//                    System.out.println("1");
-                    if (check == 2)
-                    {
-                        System.out.println(massage);
-                        System.exit(-1);
-                    }
-                    check = 1;
-                }
-
-                else if ((Objects.equals(Food[i], "I") || Objects.equals(Food[i], "II") || Objects.equals(Food[i], "III")
-                        || Objects.equals(Food[i], "IV") || Objects.equals(Food[i], "V") || Objects.equals(Food[i], "VI") || Objects.equals(Food[i], "VII")
-                        || Objects.equals(Food[i], "VIII") || Objects.equals(Food[i], "IX") || Objects.equals(Food[i], "X"))  && i != 1)
-                {
-//                    System.out.println("2");
-                    if (check == 1)
-                    {
-                        System.out.println(massage);
-                        System.exit(-2);
-                    }
-                    check = 2;
-                }
-
-                else if (/*(Objects.equals(Food[i], "+") || Objects.equals(Food[i], "-") || Objects.equals(Food[i], "*")
-                        || Objects.equals(Food[i], "/")) && */i == 1)
-                {
-//                  System.out.println("3");
-                }
-
-                else
-                {
+        for (int i = 0; i < 2; i++) {
+            if (numbs[i].contentEquals("0") || numbs[i].contentEquals("1") || numbs[i].contentEquals("2")
+                    || numbs[i].contentEquals("3") || numbs[i].contentEquals("4") || numbs[i].contentEquals("5")
+                    || numbs[i].contentEquals("6") || numbs[i].contentEquals("7") || numbs[i].contentEquals("8")
+                    || numbs[i].contentEquals("9") || numbs[i].contentEquals("10")) {
+                if (check == 2) {
                     System.out.println(massage);
-                    System.exit(-3);
+                    System.exit(-2);
                 }
-
+                check = 1;
             }
-
-        if (check == 1){
-            Arab = true;
+            else if (numbs[i].contentEquals(Book[0]) || numbs[i].contentEquals(Book[1]) || numbs[i].contentEquals(Book[2])
+                    || numbs[i].contentEquals(Book[3]) || numbs[i].contentEquals(Book[4]) || numbs[i].contentEquals(Book[5])
+                    || numbs[i].contentEquals(Book[6]) || numbs[i].contentEquals(Book[7]) || numbs[i].contentEquals(Book[8])
+                    || numbs[i].contentEquals(Book[9]) || numbs[i].contentEquals(Book[10])) {
+                if (check == 1) {
+                    System.out.println(massage);
+                    System.exit(-2);
+                }
+                check = 2;
+            }
+            else {
+                System.out.println(massage);
+                System.exit(-3);
+            }
         }
 
-        return Arab;
+        if (check == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
-
-    public static String RomanCalc (String[] Food, String[] Book, char operator){
+    public static int RomanCalc (String[] numbs, String[] Book, char operator) {
 
         int sum = 0;
-        int num1 = 2;
-        int num2 = 1;
+        int num1 = 0;
+        int num2 = 0;
 
-
-        // тут сравнить и найти из словаря перевод
-
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j <= 10; j++) {
+                if (numbs[i].contentEquals(Book[j])){
+                    switch (i){
+                        case 0:
+                            num1 = j;
+                        case 1:
+                            num2 = j;
+                    }
+                }
+            }
+        }
 
         switch (operator) {
             case '+' -> sum = num1 + num2;
@@ -125,20 +106,23 @@ public class Tester {
             case '/' -> sum = num1 / num2;
         }
 
-
-        // тут сравнить и найти из словаря перевод
-
-
-        return "String";
+        return sum;
     }
 
+    public static int ArabCalc (String[] numbs, char operator) {
 
+        int num1 = Integer.parseInt(numbs[0]);
+        int num2 = Integer.parseInt(numbs[1]);
+        int sum = 0;
+        switch (operator) {
+            case '+' -> sum = num1 + num2;
+            case '-' -> sum = num1 - num2;
+            case '*' -> sum = num1 * num2;
+            case '/' -> sum = num1 / num2;
+        }
 
+        return sum;
 
-
-
-
-
+    }
 
 }
-
